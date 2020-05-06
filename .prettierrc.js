ae3358fb9a300d8b1e8e1d7854ed9deb4dca986d
Vinicius Salvati Melquiades <vinicius.melquiades@nubank.com.br>
2020-05-05 23:44:03 -0300

    Added eslint config
    It contains some recommended settings as well as some configs from prettier

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  // printWidth: 120,
+  tabWidth: 2,
+  arrowParens: 'avoid',
+  jsxSingleQuote: true,
+  jsxBracketSameLine: false,
+};
